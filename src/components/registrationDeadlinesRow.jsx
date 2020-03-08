eda16275ea2472a8a3d893f4c3f311262d7100ba
cogpark <cogpark@gmail.com>
2020-03-08 06:57:13 -0400

    converted reg row to a function.

--- src/components/registrationDeadlinesRow.jsx
+++ src/components/registrationDeadlinesRow.jsx
@@ -2,38 +2,32 @@ import React, { Component } from "react";
 import RegistrationScorecard from './registrationScorecard';
 
 
-class RegistrationDeadlinesRow extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    handleDates = (method) => {
-        return this.props.datesData[this.props.selectedElection.toLowerCase()][this.props.selectedState][method];
-    }
-    render() {
-
-        return (
-            <React.Fragment>
-                <h2>Registration deadlines</h2>
-                <hr />
-               
-                <div className="row">
-                    <div className="col-sm"><RegistrationScorecard
-                            passJson = {this.handleDates('inPerson')}
-                            cardTitle= "In person deadline:" />
-                    </div> 
-                    <div className="col-sm middle-card"><RegistrationScorecard
-                            passJson = {this.handleDates('byMail')}
-                            cardTitle= "By mail deadline:"/>
-                    </div>
-                    <div className="col-sm"><RegistrationScorecard
-                            passJson = {this.handleDates('online')}
-                            cardTitle= "Online deadline:"/>
-                    </div>
+function RegistrationDeadlinesRow(props) { 
+    return (
+        <React.Fragment>
+            <h2>Registration deadlines</h2>
+            <hr />
+            
+            <div className="row">
+                <div className="col-sm"><RegistrationScorecard
+                        passJson = {lookupDates(props.datesData, props.selectedElection, props.selectedState,'inPerson')}
+                        cardTitle= "In person deadline:" />
+                </div> 
+                <div className="col-sm middle-card"><RegistrationScorecard
+                        passJson = {lookupDates(props.datesData, props.selectedElection, props.selectedState,'byMail')}
+                        cardTitle= "By mail deadline:"/>
                 </div>
-            </React.Fragment>
-        );
-    }
+                <div className="col-sm"><RegistrationScorecard
+                        passJson = {lookupDates(props.datesData, props.selectedElection, props.selectedState,'online')}
+                        cardTitle= "Online deadline:"/>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default RegistrationDeadlinesRow;
+
+function lookupDates(data, election, state, method) {
+    return data[election.toLowerCase()][state][method];
+}
