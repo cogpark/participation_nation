a30f11bbb7898d4c46a9d078f8f57204deffa02b
cogpark <cogpark@gmail.com>
2020-03-08 21:37:29 -0400

    Merge commit.

--- src/components/absenteeEarlyRow.jsx
+++ src/components/absenteeEarlyRow.jsx
@@ -6,10 +6,6 @@ function AbsenteeEarlyVotingRow(props) {
         <React.Fragment>
             <div>
                 <h2>Early & absentee voting</h2>
-                <ul>
-                    <li>Absentee vs. early -- if a state doesn't list requirements, we just call it "early"</li>
-                    <li>In most states, you'll follow this procedure: Fill out an absentee ballot request application, send it to your local election official, get a ballot in the mail, and send it back by election day</li>
-                </ul>
                 <hr />
             </div>
 
