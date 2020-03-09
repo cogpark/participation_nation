f0529379bf61868319aba5e45bc6c17b6faf8a13
aaron <aaaronc@gmail.com>
2020-03-09 20:58:10 +0000

    add mongoose; need to hide credentials and migrate data to db

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb+srv://partnat:2rYz6YSjYlqUuRdb@cluster0-bfrfd.mongodb.net/test?retryWrites=true&w=majority', { 
+	useUnifiedTopology: true,
+	useNewUrlParser: true
+}).then(() => {
+	console.log('connected to monogdb');
+}).catch(err => {
+	console.log('no mongodb conection: ', err);
+});
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 // should never be seen
-app.get("/", (req, res) => res.send("You hit the Participation Nation backend"));
+app.get('/', (req, res) => res.send('You hit the Participation Nation backend'));
 
-app.get("/app/feedback", (req, res) => {
-	res.send("Hi");
+app.get('/app/feedback', (req, res) => {
+	res.send('Hi');
 });
 
 // the only route we actually need
-app.post("/api/feedback", (req, res) => {
+app.post('/api/feedback', (req, res) => {
     console.log(req.body);
-    res.redirect("/");
+    res.redirect('/');
 });
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("listening on ", port));
+app.listen(port, () => console.log('listening on ', port));
