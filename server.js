7147067fac981125b4a22aa576b12e9d620d5416
Aaron Cogbill <aaaronc@gmail.com>
2020-03-09 21:56:16 -0400

    prep for heroku deploy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
 require('dotenv').config();
 
+
+// connect mongoDB
 console.log('Connecting to mongoDB...');
 mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@cluster0-bfrfd.mongodb.net/test?retryWrites=true&w=majority', { 
 	useUnifiedTopology: true,
@@ -15,13 +17,30 @@ mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@
 	console.log('no mongodb conection: ', err);
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
 
-// the only route we actually need
+// middleware -- we no longer need body-parser
+app.use(express.urlencoded({extended: true}));
+
+
+// backend routes -- eventually to be imported from a separate router dir
+// app.use('/api', api);
+//
+// for now...
 app.post('/api/feedback', (req, res) => {
     console.log(req.body);
     res.redirect('/');
 });
+
+
+// serve static assets out of client/build if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    // static routes
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 const port = process.env.PORT || 5000;
 
