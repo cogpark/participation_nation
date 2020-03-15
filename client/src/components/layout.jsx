462fb9ff7a0be7f69c6227e4315cacc9bb2bb612
cogpark <cogpark@gmail.com>
2020-03-15 17:39:11 -0400

    Fix how url renders .

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -51,10 +51,10 @@ class Layout extends Component {
     handleSelectAState(event, election) {
         this.setState({ selectedState: event.target.value,
                         onlineRegistration: RegData[election][event.target.value]['onlineRegistration']});
-        var fixElectionName = (election === "General") ? "general" : "democratic_primary"
+        var fixElectionName = (election === "general") ? "general" : "democratic_primary"
         var url = fixElectionName + '_' + event.target.value
         history.push(url)
-                    }
+        }
 
     handleSelectElectionType(event) {
         this.setState({ selectedElection: event.target.value });
@@ -64,63 +64,62 @@ class Layout extends Component {
     }
 
     render() {
-        return (
-                
-                <React.Fragment> 
-                    <div id="banner">
-                        <div className="row header-row">
-                            <div className="col-sm-4" >
-                                <img src={Title} alt='Participation Nation' style={{maxHeight:'100px'}}/>
-                            </div>
-                            <div className="col-sm-3" style={{ display:"flex", alignItems:'end'}}> 
-                                <a href="#registration-row">Registratation deadlines</a>
-                            </div>
-                            <div className="col-sm-3" style={{ display:"flex", alignItems:'end'}}> 
-                                <a href="#absentee-row">Absentee & early voting</a>
-                            </div>
+        return ( 
+            <React.Fragment> 
+                <div id="banner">
+                    <div className="row header-row">
+                        <div className="col-sm-4" >
+                            <img src={Title} alt='Participation Nation' style={{maxHeight:'100px'}}/>
+                        </div>
+                        <div className="col-sm-3" style={{ display:"flex", alignItems:'end'}}> 
+                            <a href="#registration-row">Registratation deadlines</a>
+                        </div>
+                        <div className="col-sm-3" style={{ display:"flex", alignItems:'end'}}> 
+                            <a href="#absentee-row">Absentee & early voting</a>
+                        </div>
 
+                    </div>
+                </div>
+
+                <div className="container" >
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <p>SELECT A STATE:</p>
+                            <Picker onChange={event => this.handleSelectAState(event,this.state.selectedElection.toLowerCase())} selection={this.selectedState} data={this.usStates} />
+                        </div>
+                        <div className="col-sm-4">
+                            <p>SELECT AN ELECTION:</p>
+                            <Picker onChange={event => this.handleSelectElectionType(event)} selection={this.selectedElection} data={this.electionTypes} />
+                        </div>
+                    </div>
+                    <br />
+                    <div className="row" id="registration-row">
+                        <div className="col">
+                            <RegistrationDeadlinesRow 
+                            selectedState={this.state.selectedState}
+                            selectedElection={this.state.selectedElection}
+                            datesData={this.state.datesData} />
+                        </div>
+                    </div>
+                    <div className="row padded-down">
+                        <div className="col-sm-5">
+                        <OnlineRegistrationController 
+                        selectedState = {this.state.selectedState}
+                        onlineRegistration = {this.state.onlineRegistration}/>
                         </div>
                     </div>
 
-                    <div className="container" >
-                        <div className="row">
-                            <div className="col-sm-4">
-                                <p>SELECT A STATE:</p>
-                                <Picker onChange={event => this.handleSelectAState(event,this.state.selectedElection.toLowerCase())} selection={this.selectedState} data={this.usStates} />
-                            </div>
-                            <div className="col-sm-4">
-                                <p>SELECT AN ELECTION:</p>
-                                <Picker onChange={event => this.handleSelectElectionType(event)} selection={this.selectedElection} data={this.electionTypes} />
-                            </div>
+                    <div className='row' id="absentee-row">
+                        <div className='col'>
+                            <AbsenteeEarlyVotingRow 
+                            selectedState={this.state.selectedState}
+                            selectedElection={this.state.selectedElection}
+                            absenteeData={this.state.absenteeData}
+                            />
                         </div>
-                        <br />
-                        <div className="row" id="registration-row">
-                            <div className="col">
-                                <RegistrationDeadlinesRow 
-                                selectedState={this.state.selectedState}
-                                selectedElection={this.state.selectedElection}
-                                datesData={this.state.datesData} />
-                            </div>
-                        </div>
-                        <div className="row padded-down">
-                            <div className="col-sm-5">
-                            <OnlineRegistrationController 
-                            selectedState = {this.state.selectedState}
-                            onlineRegistration = {this.state.onlineRegistration}/>
-                            </div>
-                        </div>
-
-                        <div className='row' id="absentee-row">
-                            <div className='col'>
-                                <AbsenteeEarlyVotingRow 
-                                selectedState={this.state.selectedState}
-                                selectedElection={this.state.selectedElection}
-                                absenteeData={this.state.absenteeData}
-                                />
-                            </div>
-                        </div>
-                    </div> 
-                </React.Fragment>
+                    </div>
+                </div> 
+            </React.Fragment>
         );
     }
 }
