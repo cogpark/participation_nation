a6967d17fdebdac3a2ca9ef8e76e41d63659e590
Aaron Cogbill <aaaronc@gmail.com>
2020-03-17 00:48:51 -0400

    silly error left over from testing

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const Absentee = require('./models/absentee.js');
 if (! (process.env.NODE_ENV === 'production'))
     require('dotenv').config();
 
-require('dotenv').config();
 
 // connect mongoDB
 var mongoIsConnected = false;
