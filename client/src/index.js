6f1874e1522489aadedb9fdcbe7b1228e8d9d271
cogpark <cogpark@gmail.com>
2020-08-03 09:11:05 -0400

    Fixed reloading issue.

--- client/src/index.js
+++ client/src/index.js
@@ -11,28 +11,53 @@ import * as serviceWorker from './serviceWorker';
 import CivicLitRenderer from './components/civicLitRenderer';
 
 ReactDOM.render(
-    <Router>
-        <div>
-        <Route exact path="/">
-            <App />
-        </Route>
-        <Route path="/voting">
-            <App />
-        </Route>
-        <Route path="/bill-of-rights">
-            <CivicLitRenderer url = {'BillOfRights'}/>
-        </Route>
-        <Route path="/amendments-eleven-to-twenty-seven">
-            <CivicLitRenderer url = {'AllAmendments'}/>
-        </Route>
-        <Route path="/whats-a-liberal">
-            <CivicLitRenderer url = {'WhatsALiberal'}/>
-        </Route>
-        <Route path="/whats-a-conservative">
-            <CivicLitRenderer url = {'WhatsAConservative'}/>
-        </Route>
+    <>  
+        <div class='container'>
+            <nav id="toc" class='col-sm-2' > 
+                <p class='toc-subhead'><b>VOTING INFO</b></p>
+                <div class="toc-item">
+                <a href="/voting">Voting</a>
+                </div>
+                <p class='toc-subhead'><b>WHAT IS THE...</b></p>
+                <div class="toc-item"> 
+                <a href="/bill-of-rights">Bill of Rights</a>
+                </div>
+                <div class="toc-item"> 
+                <a href="/amendments-eleven-to-twenty-seven">Amendments 11 to 27</a>
+                </div>
+                <p class='toc-subhead'><b>WHAT IS A...</b></p>
+                <div class="toc-item"> 
+                <a href="/whats-a-liberal">Liberal</a>
+                </div>
+                <div class="toc-item"> 
+                <a href="/whats-a-conservative">Conservative</a>
+                </div>
+            </nav>
+            <Router>
+                <div>
+                <Route exact path="/">
+                    <App />
+                </Route>
+                <Route path="/voting">
+                    <App />
+                </Route>
+                <Route path="/bill-of-rights">
+                    <CivicLitRenderer url = {'BillOfRights'}/>
+                </Route>
+                <Route path="/amendments-eleven-to-twenty-seven">
+                    <CivicLitRenderer url = {'AllAmendments'}/>
+                </Route>
+                <Route path="/whats-a-liberal">
+                    <CivicLitRenderer url = {'WhatsALiberal'}/>
+                </Route>
+                <Route path="/whats-a-conservative">
+                    <CivicLitRenderer url = {'WhatsAConservative'}/>
+                </Route>
+                </div>
+            </Router>
         </div>
-    </Router>, 
+        
+    </>,
     document.getElementById('root')
     );
 
