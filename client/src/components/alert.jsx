7985e7ce2ad2bf8a490df95f7cb18728fcac3158
cogpark <cogpark@gmail.com>
2020-08-17 06:08:27 -0400

    Finished!

--- client/src/components/alert.jsx
+++ client/src/components/alert.jsx
@@ -4,19 +4,13 @@ import React from 'react';
 export default function Alert(props) {
 
     return (
-        <div>
+        <div className='alert'>
             <h2>How COVID-19 affects voting {props.selectedState}</h2>
             <ul>
                {Object.keys(props.data).map(function(key) {
                    return <li value={key}>{props.data[key]}</li>
                 })}
-               
-                
-            
-
-           
             </ul>                 
-
         </div>
     )
 }
